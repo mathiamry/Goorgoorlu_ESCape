e3c2c5767b3befaf5fde8fa5473a2dafba235ff5
Cheikh G Wane <cheikhwane2007@hotmail.fr>
2017-03-20 12:12:50 +0000

    Ajout table client job et quelques attributs

--- Gg_bd.sql
+++ Gg_bd.sql
@@ -1,4 +1,5 @@
 create database Goorgoorlu;
+
 use Goorgoorlu;
 
 create table Admin(
@@ -8,11 +9,30 @@ create table Admin(
 );
 
 create table Client(
-
+    id int auto_increment,
+    username varchar(32),
+    password varchar(32),
+    numTel varchar(32),
+    localisation varchar(32),
+    constraint pk_client primary key(id)
 );
  #a discuter aek habib sur la structure de la table Goorgoorlukat
 create table Goorgoorlukat(
+    id int auto_increment,
     username varchar(32),
     password varchar(32),
+    numTel varchar(32),
+    nom varchar(32),
+    prenom varchar(32),
+    note int,
+    localisation varchar(32),
     constraint pk_user primary key(username)
+);
+
+
+create table job(
+    libelle varchar(32),
+    id_goorgoolukat int,
+    constraint pk_lib_id primary key(libelle,id_goorgoolukat),
+    constraint fk_id foreign key(id_goorgoolukat) references Goorgoorlukat(id)
 );
